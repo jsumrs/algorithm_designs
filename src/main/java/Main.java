e19a777de4b4b19763ac67029f9c3c75760d2da4
skippa-tf <github@jasoncodes.ca>
2024-03-27 19:19:14 -0700

    Implement makeArray that generates an array of random doubles.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+
 public class Main {
+
+    public static void main(String[] args) {
+        int size = 100;
+        double[] arrayOfDoubles = makeArray(100);
+
+    }
+
+    public static double[] makeArray(int size){
+        double[] arr = new double[size];
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = Math.random() * size;
+        }
+        return arr;
+    }
 }
