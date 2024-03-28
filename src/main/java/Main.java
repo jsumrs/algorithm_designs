86090108ee80c5a2b93af4739c4d220d05dfda75
skippa-tf <github@jasoncodes.ca>
2024-03-28 00:58:50 -0700

    Create a node class for use in linkedMergesort. Add a pseudocode.txt.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,3 +16,48 @@ public class Main {
         return arr;
     }
 }
+
+class Node{
+    Node prev;
+    Node next;
+
+    double[] array; // reference to the array that is being sorted.
+    int index;
+    double value;
+
+    /***
+     *
+     * @param index The index to this node's value in {@code array}.
+     * @param array The array that is being sorted.
+     */
+    public Node (int index, double[] array) {
+        next = null;
+        this.array = array;
+        this.index = index;
+        value = array[index];
+    }
+
+    /***
+     * Travel to the end of the linked list and append the node to it.
+     * @param node The node to append the to the end of the list.
+     */
+    public void append(Node node) {
+        if (next != null) {
+            next.append(node);
+        } else {
+            next = node;
+        }
+    }
+
+    /***
+     * Insert the node right before this node.
+     * @param node The node to be inserted.
+     */
+    public void insertBefore(Node node) {
+        if (prev != null){
+
+        }
+    }
+
+
+}
